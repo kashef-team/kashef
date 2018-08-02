4faf54b0cb3244f2e64545fc1b7c1fa0bd9bc31e
Mohamed Mayla <mohamed.mayla@gmail.com>
2018-08-02 20:37:51 +0300

    Develop the person edit

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,11 +5,14 @@ import restProvider from './sails-rest-provider';
 import logo from './logo.svg';
 import './App.css';
 
-import { PersonList } from './persons';
+import { PersonList, PersonCreate, PersonEdit } from './persons';
 
 const App = () => (
-  <Admin dataProvider={restProvider('http://localhost:1337')}>
-    <Resource name="person" list={PersonList} />
+  <Admin
+    dataProvider={restProvider('http://localhost:1337')}
+    title="Kashef Dashboard"
+  >
+    <Resource name="person" list={PersonList} create={PersonCreate} edit={PersonEdit} />
   </Admin>
 );
 
--- dashboard/src/persons/index.js
+++ dashboard/src/persons/index.js
@@ -1,11 +1,67 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { 
+    List,
+    Datagrid,
+    TextField,
+    Create,
+    SimpleForm,
+    SimpleFormIterator,
+    TextInput,
+    ArrayInput,
+    SelectField,
+    BooleanInput,
+    Edit,
+    EditButton,
+    DisabledInput,
+ } from 'react-admin';
 
 export const PersonList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
             <TextField source="name" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PersonCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+            <ArrayInput source="images">
+                <SimpleFormIterator>
+                    <TextInput source="url" />
+                </SimpleFormIterator>
+            </ArrayInput>
+            <SelectField source="gender" choices={[
+                { id: 'male', name: 'Male' },
+                { id: 'female', name: 'Female' },
+            ]} />
+            <BooleanInput label="is soldier?" source="isSoldier" />
+        </SimpleForm>
+    </Create>
+);
+
+const PersonTitle = ({ record }) => {
+    return <span>Person id: {record ? `"${record.id}"` : ''}</span>;
+};
+
+export const PersonEdit = (props) => (
+    <Edit title={<PersonTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput label="Id" source="id" />
+            <TextInput source="name" />
+            <ArrayInput source="images">
+                <SimpleFormIterator>
+                    <TextInput source="url" />
+                </SimpleFormIterator>
+            </ArrayInput>
+            <SelectField source="gender" choices={[
+                { id: 'male', name: 'Male' },
+                { id: 'female', name: 'Female' },
+            ]} />
+            <BooleanInput label="is soldier?" source="isSoldier" />
+        </SimpleForm>
+    </Edit>
 );

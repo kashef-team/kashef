2461fab24d6a2d550b976ee633820f9a6b840cfc
omarseifelnasr <42005423+omarseifelnasr@users.noreply.github.com>
2018-08-03 02:17:11 +0300

    Client Updated

---
+++ image-processing/Kashef.Services/AmazonClient.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Rekognition;
+using Amazon.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashef.Services
+{
+    public static class AmazonClient
+    {
+        public static AmazonRekognitionClient GetInstance()
+        {
+            BasicAWSCredentials credentials = new BasicAWSCredentials("ID", "Value");
+
+            return new AmazonRekognitionClient(credentials, Amazon.RegionEndpoint.USWest2);
+
+        }
+    }
+}

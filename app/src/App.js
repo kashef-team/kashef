73b239bbb95eb236a43f4087b793554896bd7d8c
Mohamed Mayla <mohamed.mayla@gmail.com>
2018-08-03 03:47:48 +0300

    Create Top App Bar

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import TopAppBar from './TopAppBar';
 
 import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <TopAppBar />
     );
   }
 }
